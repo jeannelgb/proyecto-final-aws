770c4caba6a88f98e1d460e73620f27f017dfb98
jeannelgb <jeanne.lgb@free.fr>
2025-11-12 19:25:32 -0600

    correction app.js 2

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ app.use((err, req, res, next) => {
 
 // Démarrage
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Servidor en línea en http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`✅ Servidor en línea en http://localhost:${PORT}`));
