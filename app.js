4f1a5f5283f8c8e59a9d0f92f9f85f8540710c00
jeannelgb <jeanne.lgb@free.fr>
2025-11-12 20:38:51 -0600

    correction commentaires

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const profesoresRoutes = require('./routes/profesores.js');
 const app = express();
 app.use(bodyParser.json());
 
-// Routes
+// Rutas
 app.use('/alumnos', alumnosRoutes);
 app.use('/profesores', profesoresRoutes);
 
-// Route de test
+// Ruta de validación
 app.get('/', (req, res) => {
   res.json({ message: 'API REST de Alumnos y Profesores funcionando correctamente.' });
 });
 
-// Gestion d’erreurs générales
+// Gestión de errores generales
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Error interno del servidor.' });
 });
 
-// Démarrage
+// Inicio
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => console.log(`✅ Servidor en línea en http://localhost:${PORT}`));
